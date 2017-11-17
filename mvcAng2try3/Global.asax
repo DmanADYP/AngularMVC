c4d0aafadd9475d57385031e537c434e56bc8e2d
Drew Pennetti <Drew.Pennetti@pb.com>
2017-11-17 10:38:18 -0500

    First commit

---
+++ mvcAng2try3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="mvcAng2try3.MvcApplication" Language="C#" %>
---
+++ mvcAng2try3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mvcAng2try3.Startup))]
+namespace mvcAng2try3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
